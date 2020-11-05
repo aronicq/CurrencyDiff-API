e6fa06c203a33f1769fe2cf845b2fdcd7b251cb7
user <efim002@gmail.com>
2020-11-05 16:26:34 +0300

    added docker and uwsgi configurations

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM alpine
+
+RUN apk add --no-cache uwsgi-python3 python3 py3-pip python3-dev
+RUN apk add --update --no-cache g++ gcc libxml2-dev libxslt-dev
+
+RUN pip3 install flask
+RUN pip3 install requests
+RUN pip3 install bs4
+RUN pip3 install lxml
+
+COPY flask_app /code
+
+CMD cd code && uwsgi --ini uwsgi.ini
--- flask_app/app.py
+++ flask_app/app.py
@@ -40,9 +40,9 @@ def get_difference():
     return {"start_rate": start_rate,
             "end_rate": end_rate,
             "rate_difference": start_rate - end_rate}
-
-
-if __name__ == '__main__':
-    app.run(host="0.0.0.0")
-# print(get_difference("01-10-2000", "01-11-2020", "EUR"))
-# print(convert_date("2000-10-02"))
+#
+#
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0")
+# # print(get_difference("01-10-2000", "01-11-2020", "EUR"))
+# # print(convert_date("2000-10-02"))
